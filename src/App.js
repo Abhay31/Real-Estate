838cc8b5ee961dd80dba9649a95b5b987c0aefa5
Abhay31 <abhaysharma0983@gmail.com>
2023-12-26 21:04:48 +0530

    Updated footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './Footer/Footer';
 import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
